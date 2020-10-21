a6980cef326d1f7ba5bf66805a0717f131085cf1
forw1k <52047879+forw1k@users.noreply.github.com>
2020-10-21 19:52:44 +0200

    Updates

---
+++ precache-manifest.2e5ae843197cebe28e5bd9134475c0bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c2ed2b1945a01c5ad241a174d27cc5e",
+    "url": "/ru-you-app/index.html"
+  },
+  {
+    "revision": "e6ac2fdd845aec08de33",
+    "url": "/ru-you-app/static/css/main.06e257a4.chunk.css"
+  },
+  {
+    "revision": "319d1bbf2a075ce6a4eb",
+    "url": "/ru-you-app/static/js/2.6934a705.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/ru-you-app/static/js/2.6934a705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ac2fdd845aec08de33",
+    "url": "/ru-you-app/static/js/main.c5679608.chunk.js"
+  },
+  {
+    "revision": "b826ba924b7c0c43b486",
+    "url": "/ru-you-app/static/js/runtime-main.eb7550ed.js"
+  },
+  {
+    "revision": "b20ea88a511a0e3e6dcccddff65b3fe2",
+    "url": "/ru-you-app/static/media/icons.b20ea88a.svg"
+  }
+]);
